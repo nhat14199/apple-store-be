b69da660aa11bf56885d1242a2ab44555adfe1d7
nguyenvannhat <nguyenvannhat14199@gmail.com>
2024-07-23 23:23:16 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*', // Hoặc bạn có thể chỉ định nguồn cụ thể
+  });
   // await app.listen(3003, () => console.log('connect success '));
   await app.listen(process.env.PORT || 3000, () =>
     console.log('connect success ', process.env.MONGODB_URI),
